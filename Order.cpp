cb732f570a5cec3cadeaf18f929eb7cc26765509
Anthony Nguyen <86622824+anthonynguyen1575@users.noreply.github.com>
2021-12-16 00:09:46 -0800

    Update Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -1,20 +1,23 @@
 #include "Order.h"
+#include <iostream>
 
-Order::Order(std::vector<Item> order)
-	: myOrder { order } { 
-}
+Order::Order(std::vector<Item> order, std::vector<int> amountForEachItem)
+	: myOrder{ order }, myItemCount{ amountForEachItem } { }
 
-void Order::addItems(Item item) {
+void Order::addItems(Item item, int amount) {
 	myOrder.push_back(item);
+	myItemCount.push_back(amount);
 }
 
 double getTotalPrice(Order order) {
 	std::vector<Item> items = order.myOrder;
+	std::vector<int> amount = order.myItemCount;
 	double total = 0;
 
 	for (std::size_t i = 0; i < items.size(); i++) {
 		Item item = items[i];
-		total += item.myItemPrice;
+		int count = amount[i];
+		total += item.myItemPrice * count;
 	}
 
 	return total;
