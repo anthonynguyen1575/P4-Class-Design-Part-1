673faa6baa0942c2f2910955e1b025b995209b5f
Anthony Nguyen <86622824+anthonynguyen1575@users.noreply.github.com>
2021-12-16 00:09:29 -0800

    Update Order.h

--- Order.h
+++ Order.h
@@ -3,11 +3,12 @@
 
 class Order {
 public:
-	Order(std::vector<Item> order);
+	Order(std::vector<Item> order, std::vector<int> amountForEachItem);
 
 	std::vector<Item> myOrder;
+	std::vector<int> myItemCount;
 
-	void addItems(Item item);
+	void addItems(Item item, int amount);
 };
 
 double getTotalPrice(Order order);
